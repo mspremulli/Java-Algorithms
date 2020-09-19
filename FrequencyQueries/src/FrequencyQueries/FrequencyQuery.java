273209c74d9d7a2f3ae37068e8c3f4d50ea479f0
Michael Spremulli <michaels.wpi@gmail.com>
2020-09-19 01:31:28 -0400

    fixed error where count could go negative

--- FrequencyQueries/src/FrequencyQueries/FrequencyQuery.java
+++ FrequencyQueries/src/FrequencyQueries/FrequencyQuery.java
@@ -3,30 +3,24 @@ import java.util.*;
 public class FrequencyQuery {
 
   static List<Integer> freqQuery(List<List<Integer>> queries) {
-    HashMap frequencyHashmap = new HashMap();
-
-
+    Map frequencyHashmap = new HashMap();
     List answer = new ArrayList <Integer>();
 
     for (List<Integer> query : queries) {
       int first = query.get(0);
       int second = query.get(1);
-//      System.out.println(Collections.singletonList(frequencyHashmap));
-      switch (first) {
-        case 1:
-          if (frequencyHashmap.containsKey(second)) {
-            frequencyHashmap.put(second, (int) frequencyHashmap.get(second) + 1);
-          } else {
-            frequencyHashmap.put(second, 1);
 
-          }
+      switch (first) {
+        case 1: //add the item to the map or increment the count if its there
+          if (frequencyHashmap.containsKey(second))
+            frequencyHashmap.put(second, (int) frequencyHashmap.get(second) + 1);
+          else frequencyHashmap.put(second, 1);
           break;
-        case 2:
-          if (frequencyHashmap.containsKey(second)) {
+        case 2: //if item is in the map, remove 1 from it
+          if (frequencyHashmap.containsKey(second) && (int) frequencyHashmap.get(second) != 0)
             frequencyHashmap.put(second, (int) frequencyHashmap.get(second) - 1);
-          }
           break;
-        case 3:
+        case 3: //add 1 or 0 to the answer array if the frequency occurs
           answer.add(frequencyHashmap.containsValue(second) ? 1 : 0);
           break;
         default:
@@ -35,7 +29,7 @@ public class FrequencyQuery {
     }
 
 //    System.out.println(Collections.singletonList(frequencyHashmap));
-//    printArrayList(answer);
+    printArrayList(answer);
     return answer;
 
   }
@@ -53,6 +47,11 @@ public class FrequencyQuery {
     testArray1.add( new ArrayList<>(Arrays.asList(3,2)));
 
     freqQuery(testArray1);
+  }
+
+  public static ArrayList<Integer> createTest(int[] numbers){
+
+    return null;
   }
 
   public static void printArrayList(List list){
