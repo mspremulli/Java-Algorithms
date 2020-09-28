4a49a7e7bc3c4ebc58449660e09470de434f290b
Michael Spremulli <michaels.wpi@gmail.com>
2020-09-27 23:02:50 -0400

    added check notice and fixed bugs

--- FradulentActivity/src/FraudulentActivity.java
+++ FradulentActivity/src/FraudulentActivity.java
@@ -5,9 +5,9 @@ public class FraudulentActivity {
   public static void main(String[] args){
     System.out.println(activityNotifications(new int[] {2, 3, 4, 2, 3, 6, 8, 4, 5} , 5));
     System.out.println(activityNotifications(new int[] {10, 20, 30, 40, 50} , 3));
+    System.out.println(activityNotifications(new int[] {1, 2, 3, 4, 4}, 4));
   }
 
-  // Complete the activityNotifications function below.
   static int activityNotifications(int[] expenditure, int numberOfDays) {
     int noticeCount = 0;
     ArrayList<Integer> trailingDays = new ArrayList<>();
@@ -16,43 +16,31 @@ public class FraudulentActivity {
     for (int i = 0; i < numberOfDays ; i++) {
       trailingDays.add(i, expenditure[i]);
     }
-    //sort
-    Collections.sort(trailingDays);
-    //remove oldest item
+    Collections.sort(trailingDays);//Sort
 
-    //add newest item
     for (int i = numberOfDays; i < expenditure.length; i++) {
-      insertItem(trailingDays, expenditure[i]);
+      if(expenditure[i] >= 2 * calculateMedian(trailingDays)) noticeCount++;//check and count notice
 
-    System.out.println(Collections.singletonList(trailingDays));
+      trailingDays.remove((Integer) expenditure[i - numberOfDays]); //remove oldest item
+      insertItem(trailingDays, expenditure[i]);//add newest item
     }
-
     return noticeCount;
   }
 
-  static float calculateMedian(List<Integer> numbers) {
+  static double calculateMedian(ArrayList<Integer> numbers) {
     int length = numbers.size();
     if(length % 2 == 1){
       return numbers.get(length / 2);
     }
-    return (numbers.get((length / 2) + numbers.get(length/2 -1))) / 2;
+    return   (numbers.get(length / 2) + numbers.get(length / 2 - 1)) / 2.0;
   }
 
-  public static ArrayList<Integer> insertItem(ArrayList list, int numberToAdd){
+  public static void insertItem(ArrayList<Integer> list, int numberToAdd){
       int j = 0;
       while (j < list.size()){
-        if((int) list.get(j) >= numberToAdd) break;
+        if(list.get(j) >= numberToAdd) break;
         j++;
       }
-
       list.add(j, numberToAdd);
-      return list;
-  }
-
-  public static void printArrayList(List<Integer> list){
-    for (Object o : list) {
-      System.out.print(o + " ");
-    }
-    System.out.println();
   }
 }
