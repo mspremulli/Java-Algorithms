da99c8af188e4eb589fba35c46cde6b37f4c6a3e
Michael Spremulli <michaels.wpi@gmail.com>
2020-09-27 23:19:40 -0400

    added variables to reduce duplicate calculations

--- FradulentActivity/src/FraudulentActivity.java
+++ FradulentActivity/src/FraudulentActivity.java
@@ -19,20 +19,22 @@ public class FraudulentActivity {
     Collections.sort(trailingDays);//Sort
 
     for (int i = numberOfDays; i < expenditure.length; i++) {
-      if(expenditure[i] >= 2 * calculateMedian(trailingDays)) noticeCount++;//check and count notice
-
+      int newItem = expenditure[i];
+      if(newItem >= 2 * calculateMedian(trailingDays)) noticeCount++;//check and count notice
       trailingDays.remove((Integer) expenditure[i - numberOfDays]); //remove oldest item
-      insertItem(trailingDays, expenditure[i]);//add newest item
+      insertItem(trailingDays, newItem);//add newest item
     }
     return noticeCount;
   }
 
-  static double calculateMedian(ArrayList<Integer> numbers) {
+  static float calculateMedian(ArrayList<Integer> numbers) {
     int length = numbers.size();
-    if(length % 2 == 1){
-      return numbers.get(length / 2);
-    }
-    return   (numbers.get(length / 2) + numbers.get(length / 2 - 1)) / 2.0;
+    int midIndexValue = numbers.get(length / 2);
+
+    if(length % 2 == 1) return midIndexValue;
+
+    int midIndexValue2 = numbers.get((length / 2) - 1);
+    return (float) ((midIndexValue + midIndexValue2) / 2.0);
   }
 
   public static void insertItem(ArrayList<Integer> list, int numberToAdd){
