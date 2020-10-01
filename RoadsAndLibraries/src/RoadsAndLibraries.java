6944b8801ba8ea72bc5ec00c499505886faa1997
Michael Spremulli <michaels.wpi@gmail.com>
2020-09-30 23:32:11 -0400

    created a hashmap to store the cities and their values

--- RoadsAndLibraries/src/RoadsAndLibraries.java
+++ RoadsAndLibraries/src/RoadsAndLibraries.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class RoadsAndLibraries {
   public static void main(String[] args){
-
+    roadsAndLibraries(6, 2, 3, new int[][] {{1, 2}, {1, 3}, {4, 5}, {4, 6}});
   }
 
   static long roadsAndLibraries(int numberOfCities, int libraryCost, int roadCost, int[][] cities) {
@@ -8,7 +12,14 @@ public class RoadsAndLibraries {
     int librariesBuilt = 0;
 
     //decide what roads and libraries to rebuild
+    HashMap<Integer, ArrayList<Integer>> cityMap = new HashMap<>();
 
+    for (int i = 1; i <= numberOfCities; i++) {
+      ArrayList<Integer> cityList = new ArrayList<>();
+      cityList.add(i);
+      cityMap.put(i, cityList);
+    }
+    System.out.println(Collections.singletonList(cityMap));
 
     return roadsRepaired * roadCost + librariesBuilt * libraryCost;
   }
