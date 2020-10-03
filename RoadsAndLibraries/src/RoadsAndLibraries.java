62e35732c29d99a7df3b067be680a3eeb64dfcd4
Michael Spremulli <michaels.wpi@gmail.com>
2020-10-02 22:12:58 -0400

    fixed hashmap for cities bug

--- RoadsAndLibraries/src/RoadsAndLibraries.java
+++ RoadsAndLibraries/src/RoadsAndLibraries.java
@@ -4,23 +4,31 @@ import java.util.HashMap;
 
 public class RoadsAndLibraries {
   public static void main(String[] args){
-    roadsAndLibraries(6, 2, 3, new int[][] {{1, 2}, {1, 3}, {4, 5}, {4, 6}});
+    roadsAndLibraries(4,2, 3, new int[][] {{1, 2}, {1, 3}, {4, 5}, {4, 6}});
   }
 
   static long roadsAndLibraries(int numberOfCities, int libraryCost, int roadCost, int[][] cities) {
     int roadsRepaired = 0;
     int librariesBuilt = 0;
 
-    //decide what roads and libraries to rebuild
-    HashMap<Integer, ArrayList<Integer>> cityMap = new HashMap<>();
+    //if libraries cost more than roads, build one in every city
+    if(libraryCost > roadCost) return numberOfCities * libraryCost;
 
-    for (int i = 1; i <= numberOfCities; i++) {
+
+    //build each enclosed city unit
+    HashMap<Integer, ArrayList<Integer>> cityMap = new HashMap<>();
+    for (int i = 0; i < numberOfCities; i++) {
       ArrayList<Integer> cityList = new ArrayList<>();
-      cityList.add(i);
+      cityList.add(cities[i][0]);
       cityMap.put(i, cityList);
     }
     System.out.println(Collections.singletonList(cityMap));
 
-    return roadsRepaired * roadCost + librariesBuilt * libraryCost;
+
+
+    int totalRoadCost = roadsRepaired * roadCost;
+    int totalLibraryCost =  librariesBuilt * libraryCost;
+
+    return totalRoadCost + totalLibraryCost;
   }
 }
