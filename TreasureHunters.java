1443671f774b8201b39861fa0174a4539e1715c6
Michael Spremulli <michaels.wpi@gmail.com>
2020-10-08 12:50:50 -0400

    TreasureHunters algorithm added traps

--- TreasureHunters.java
+++ TreasureHunters.java
@@ -5,10 +5,11 @@ public class TreasureHunters {
 //    x = 2 y = 2 instructions: [[2, 3], [1, 1], [0, 2], [1, 2], [2, 4], [3, 1]]
     int[] startingPoint = new int[] {2,2};
     int[][] instructions = new int[][] {{2, 3}, {1, 1}, {0, 2}, {1, 2}, {2, 4}, {3, 1}};
-    System.out.println(Arrays.toString(findTreasure(startingPoint, instructions)));
+    int[][] traps = new int[][] {{2, 5}, {0,1}, {3, 2}};
+    System.out.println(Arrays.toString(findTreasure(startingPoint, instructions, traps)));
   }
 
-  public static int[] findTreasure(int[] location, int[][] instructions){
+  private static int[] findTreasure(int[] location, int[][] instructions, int[][] traps){
     // 0 = north
     // 1 = east
     // 2 = south
@@ -30,8 +31,20 @@ public class TreasureHunters {
         default:
           break;
       }
-      System.out.println(location[0] + " " + location[1]);
+
+      if(isOnATrap(traps, location)){
+        return new int[]{-1,-1};
+      }
     }
     return location;
+  }
+
+  private static boolean isOnATrap(int[][] traps, int[] location){
+    for (var trap : traps) {
+      if(trap[0] == location[0] && trap[1] == location[1]){
+        return true;
+      }
+    }
+    return false;
   }
 }
