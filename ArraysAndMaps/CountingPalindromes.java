fef31e6fa8077980c30b09babe2521c16c39b3b0
Michael Spremulli <michaels.wpi@gmail.com>
2020-11-17 21:49:51 -0500

    Counting Palindromes

---
+++ ArraysAndMaps/CountingPalindromes.java
@@ -0,0 +1,32 @@
+package ArraysAndMaps;
+
+public class CountingPalindromes {
+
+  public static int countPalindromes(String s) {
+    int length = s.length();
+    int counter = length;
+
+    for (int i = 0; i < length; i++) {
+      for (int j = i; j < length - 1; j++) {
+        if(isPalindrome(s.substring(i,length - j + i))) counter++;
+      }
+    }
+
+    return counter;
+  }
+
+  public static boolean isPalindrome(String str){
+    int length = str.length() - 1;
+    for(int i = 0; i <= length / 2; i++){
+      if(str.charAt(i) != str.charAt(length - i)){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static void main(String [] args){
+    System.out.println(countPalindromes("tacocat"));
+    System.out.println(countPalindromes("aaa"));
+  }
+}
