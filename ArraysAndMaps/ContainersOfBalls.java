483d72eeb70a3c587efe6d7036115fbf10d7efd9
Michael Spremulli <michaels.wpi@gmail.com>
2020-11-20 17:23:08 -0500

    Containers of balls

--- ArraysAndMaps/ContainersOfBalls.java
+++ ArraysAndMaps/ContainersOfBalls.java
@@ -1,7 +1,7 @@
 package ArraysAndMaps;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 
 public class ContainersOfBalls {
 
@@ -9,33 +9,34 @@ public class ContainersOfBalls {
     final String POSSIBLE = "Possible";
     final String IMPOSSIBLE = "Impossible";
 
-    HashMap<Integer,Integer> containerCount = new HashMap<>();
-    HashMap<Integer,Integer> ballTypeCount = new HashMap<>();
+    ArrayList<Integer> containerCount = new ArrayList<>(Collections.nCopies(container.length,0));
+    ArrayList<Integer> ballTypeCount = new ArrayList<>(Collections.nCopies(container.length,0));
 
-    int index = 0;
-    for (int[] containers : container ) {
-      containerCount.put(index++, containers.length);
+    for (int i = 0; i < container.length; i++) {
+      for (int j = 0; j < container[i].length; j++) {
+        containerCount.set(i, containerCount.get(i)+ container[i][j]);
+        ballTypeCount.set(j, ballTypeCount.get(j) + container[i][j]);
+      }
+    }
+    Collections.sort(ballTypeCount);
+    Collections.sort(containerCount);
 
-      for (int ballType : containers ) {
-        if (!ballTypeCount.containsKey(ballType)) ballTypeCount.put(ballType, 1);
-        else ballTypeCount.put(ballType, ballTypeCount.get(ballType) + 1);
+    boolean isImpossible = false;
+    for (int i = 0; i < ballTypeCount.size(); i++) {
+      if(!ballTypeCount.get(i).equals(containerCount.get(i))) {
+        isImpossible = true;
+        break;
       }
     }
 
-    System.out.println(containerCount);
-    System.out.println(ballTypeCount);
-
-    for (var containerSize : containerCount.keySet()) {
-      System.out.println(containerSize.getClass());
-    }
-
+    if(isImpossible) return IMPOSSIBLE;
     return POSSIBLE;
   }
 
   public static void main(String[] args){
     int[][] containers = new int[][]{{0,2,1}, {1,2,1}, {2,0,0}};
     System.out.println(organizingContainers(containers));
-    int[][] containers2 = new int[][]{{0,2,1}, {1,1,1}, {2,0,0}};
+    int[][] containers2 = new int[][]{{999336263, 998799923}, {998799923, 999763019}};
     System.out.println(organizingContainers(containers2));
   }
 }
