1b3bcecf417dafbfc978f2ee738a55330067b3be
Michael Spremulli <michaels.wpi@gmail.com>
2020-11-21 03:26:30 -0500

    fixed edge case for impossible values

--- ArraysAndMaps/Bonetrousle.java
+++ ArraysAndMaps/Bonetrousle.java
@@ -6,22 +6,23 @@ public class Bonetrousle {
   static long[] bonetrousle(long numOfSticks, long storeBoxes, int boxesToBuy) {
     long[] answer = new long[boxesToBuy];
     int index = 0;
+    int sumCheck = 0;
+    long stickCheck = numOfSticks;
     long minSticks = (boxesToBuy * (1 + boxesToBuy) / 2);
 
-//      System.out.println(numOfSticks +" "+ minSticks);
     if(numOfSticks > (storeBoxes * (1 + storeBoxes) / 2)) return new long[]{-1};
 
     while(boxesToBuy > 0 && storeBoxes > 0) {
-//      System.out.println(numOfSticks +" "+ storeBoxes  +" "+ minSticks  +" "+ boxesToBuy);
       if(numOfSticks - storeBoxes >= minSticks - boxesToBuy){
-//        System.out.print(storeBoxes + " ");
         answer[index++] = storeBoxes;
+        sumCheck += storeBoxes;
         numOfSticks -= storeBoxes;
         minSticks -= boxesToBuy--;
       }
       storeBoxes--;
-//      System.out.println("min: " + minSticks +"   numsticks: "+ numOfSticks + "   boxestobuy: " + boxesToBuy);
     }
+
+    if(sumCheck < stickCheck) return new long[]{-1};
     return answer;
   }
 
@@ -29,7 +30,7 @@ public class Bonetrousle {
   public static void main(String[] args){
     System.out.println(Arrays.toString(bonetrousle(10, 3, 3))); //8,3,1
     System.out.println(Arrays.toString(bonetrousle(9, 10, 2)));//8,1
-//    System.out.println(Arrays.toString(bonetrousle(10, 3, 3)));//3,2,1 impossible
+    System.out.println(Arrays.toString(bonetrousle(10, 3, 3)));//3,2,1 impossible
   }
 
 }
