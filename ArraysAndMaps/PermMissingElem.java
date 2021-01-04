ad59e0a2496add0681444357e5c9f5ddb13fdbe5
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-04 18:47:33 -0500

    perm missing element

---
+++ ArraysAndMaps/PermMissingElem.java
@@ -0,0 +1,21 @@
+package ArraysAndMaps;
+
+import java.util.Arrays;
+
+public class PermMissingElem {
+  public static int solution(int[] A) {
+    Arrays.sort(A);
+    int len = A.length;
+    for(int i = 0; i < len; i++) {
+      // System.out.println(i+1 + " " + A[i]);
+      if(i+1 != A[i]) return i+1;
+    }
+
+    // write your code in Java SE 8
+    return len + 1;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(new int[]{1,3,2,5,6}));
+  }
+}
