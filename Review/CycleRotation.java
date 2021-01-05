06800f8c44ae5968e0233ff22d031a4e1e9301d2
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-04 23:51:52 -0500

    Cycle rotation

---
+++ Review/CycleRotation.java
@@ -0,0 +1,26 @@
+package Review;
+
+import java.util.Arrays;
+
+public class CycleRotation {
+  public static int[] solution(int[] a, int k) {
+    int[] rotated = new int[a.length];
+    int len = a.length;
+    int newK = k % a.length;
+
+    for (int i = 0; i < len; i++) {
+      if(i - newK >= 0){
+        rotated[i] = a[i-newK];
+      }
+      else{
+        rotated[i] = a[i-newK + len];
+      }
+    }
+
+    return rotated;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Arrays.toString(solution(new int[]{3, 4, 5, 6, 7}, 5)));
+  }
+}
