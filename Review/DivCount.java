b153959d912c853c572d08d348ef92ae8ec12b40
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-09 03:53:54 -0500

    Div count changed to faster math solution

--- Review/DivCount.java
+++ Review/DivCount.java
@@ -2,15 +2,14 @@ package Review;
 
 public class DivCount {
   public static int solution(int A, int B, int K) {
-   int numOfDivisible = 0;
-   int start = Math.max(A,K);
-    for (int i = start; i <= B ; i++) {
-      if(i % K == 0) numOfDivisible++;
-    }
-   return numOfDivisible;
+    return (int) (Math.floor(B / (double) K) - Math.ceil(A / (double) K) + 1);
   }
 
   public static void main(String[] args) {
     System.out.println(solution(6,11,2));
+    System.out.println(solution(11,345,17));
+    System.out.println(solution(0,1,11));
+    System.out.println(solution(10,10,5));
+    System.out.println(solution(10,10,7));
   }
 }
