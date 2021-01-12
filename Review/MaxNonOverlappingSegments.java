8f414470447854879fd7e7e616ce526c90ba83ea
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-11 23:21:31 -0500

    Max non-overlapping segments

---
+++ Review/MaxNonOverlappingSegments.java
@@ -0,0 +1,19 @@
+package Review;
+
+public class MaxNonOverlappingSegments {
+  public static int solution(int[] A, int[] B) {
+    int maxSet = 0;
+    int previousEnd = 0;
+    for (int i = 0; i < A.length; i++) {
+      if(A[i] > previousEnd){
+        previousEnd = B[i];
+        maxSet++;
+      }
+    }
+    return maxSet;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(new int[]{1,3,7,9,9}, new int[]{5,6,8,9,10}));
+  }
+}
