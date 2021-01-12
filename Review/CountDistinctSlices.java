7e707d304dfbc73da0253b2cc1ffec3a5497e611
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-11 23:57:17 -0500

    count distinct slices

---
+++ Review/CountDistinctSlices.java
@@ -0,0 +1,27 @@
+package Review;
+
+import java.util.HashSet;
+
+public class CountDistinctSlices {
+  public static int solution(int M, int[] A) {
+    int distinctSlices = 0;
+    HashSet<Integer> currentSlices = new HashSet<>();
+
+    for (int num : A) {
+      if(currentSlices.contains(num)){
+        distinctSlices += AdditiveFactorial(currentSlices.size());
+        currentSlices.clear();
+      }
+      currentSlices.add(num);
+    }
+    return distinctSlices + AdditiveFactorial(currentSlices.size());
+  }
+
+  public static int AdditiveFactorial(int num){
+    return (int) ((num + 1) * (double)(num / 2.0));
+  }
+
+  public static void main(String[] args) {
+    System.out.println(solution(6, new int[]{3,4,5,5,2}));
+  }
+}
