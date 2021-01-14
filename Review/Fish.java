99393ed3db8e507bb15e836a10cac725ae966381
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-13 19:40:17 -0500

    fixed edge case

--- Review/Fish.java
+++ Review/Fish.java
@@ -38,5 +38,7 @@ public class Fish {
 
   public static void main(String[] args) {
     System.out.println(solution(new int[]{4,3,2,1,5}, new int[]{0,1,0,0,0}));
+    System.out.println(solution(new int[]{4}, new int[]{0}));
+    System.out.println(solution(new int[]{0}, new int[]{1}));
   }
 }
