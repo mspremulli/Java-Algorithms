b3e2cdfd38305d449508007c9c7c7bd21d90dc64
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-16 20:19:34 -0500

    optimized runtime

--- Review/MaxCounters.java
+++ Review/MaxCounters.java
@@ -6,16 +6,19 @@ public class MaxCounters {
   public static int[] solution(int N, int[] A) {
     int[] counters = new int[N];
     int maxCounter = 0;
+    int beforeCount = 0;
 
     for (int num : A) {
-      if(num <= N) {
-        counters[num - 1]++;
-        if(counters[num - 1] > maxCounter) maxCounter = counters[num - 1];
-      }
-      else {
-        Arrays.fill(counters,maxCounter);
-      }
+      if(num > N) beforeCount = maxCounter;
+      else if(counters[num - 1] < beforeCount) counters[num - 1] = beforeCount + 1;
+      else counters[num - 1]++;
+
+      if(num < N && counters[num - 1] > maxCounter) maxCounter = counters[num - 1];
     }
+    for (int i = 0; i < counters.length; i++) {
+      if(counters[i] < beforeCount) counters[i] = beforeCount;
+    }
+
     return counters;
   }
 
