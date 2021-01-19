45cac973fede406da07446af67904ef5e952e354
Michael Spremulli <michaels.wpi@gmail.com>
2021-01-18 22:53:38 -0500

    switched to a stack

--- Review/BalancedBrackets.java
+++ Review/BalancedBrackets.java
@@ -1,21 +1,49 @@
 package Review;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class BalancedBrackets {
 
-  static String isBalanced(String s) {
-    String oldS = "";
-    while(s.length() > 1 && !oldS.equals(s)){
-      oldS = s;
-      s = s.replaceAll("\\(\\)", "");
-      s = s.replaceAll("\\{}", "");
-      s = s.replaceAll("\\[]", "");
+  static int solution(String s) {
+//    String oldS = "";
+//    while(s.length() > 1 && !oldS.equals(s)){
+//      oldS = s;
+//      s = s.replaceAll("\\(\\)", "");
+//      s = s.replaceAll("\\{}", "");
+//      s = s.replaceAll("\\[]", "");
+//
+//    }
+//    if(s.length() == 0) return "YES";
+//    return "NO";
 
+    Stack<String> stack = new Stack<>();
+    for (String bracket: s.split("") ) {
+      switch (bracket){
+        case "[":
+        case "{":
+        case "(":
+          stack.add(bracket);
+          break;
+        case "]":
+          if(!stack.pop().equals("[")) return 0;
+          break;
+        case "}":
+          if(!stack.pop().equals("{")) return 0;
+          break;
+        case ")":
+          if(!stack.pop().equals("(")) return 0;
+          break;
+
+        default:
+          return 0;
+      }
     }
-    if(s.length() == 0) return "YES";
-    return "NO";
+    return 1;
   }
 
   public static void main(String[] args){
-    System.out.println(isBalanced("{[()]}"));
+    System.out.println(solution("{[(]}"));
+    System.out.println(solution("{[()]}"));
   }
 }
